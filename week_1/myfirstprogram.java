d98aaf2d97108b955ae629258731e23f279171d5
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-17 21:58:50 +0800

    Create myfirstprogram.java

---
+++ week_1/myfirstprogram.java
@@ -0,0 +1,8 @@
+// Class name should be the same with the file name
+// This program should have a name of Main.java
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("This is my first Java Program!");
+  }
+}
