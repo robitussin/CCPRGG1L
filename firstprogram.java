a5f358b29f6e528a415c70bc73c8bfae846bc5e8
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-17 21:31:36 +0800

    Update firstjavaprogram
    Added comments

--- firstprogram.java
+++ firstprogram.java
@@ -1,3 +1,6 @@
+// Class name should be the same with the file name
+// This program should have a name of Main.java
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("This is my first Java Program!");
