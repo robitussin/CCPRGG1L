b4aadc8ac90ffb7ea64eeb439a7b0c86b1226b8d
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-17 21:46:30 +0800

    Update firstprogram.java
    Changed Main to App

--- firstprogram.java
+++ firstprogram.java
@@ -1,7 +1,7 @@
 // Class name should be the same with the file name
 // This program should have a name of Main.java
 
-public class Main {
+public class App {
   public static void main(String[] args) {
     System.out.println("This is my first Java Program!");
   }
