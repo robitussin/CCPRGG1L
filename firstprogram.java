53abefb9c2a1659e15c701dae2c3aeb5aea10c38
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-17 21:59:47 +0800

    Delete firstprogram.java

--- firstprogram.java
+++
@@ -1,8 +0,0 @@
-// Class name should be the same with the file name
-// This program should have a name of Main.java
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("This is my first Java Program!");
-  }
-}
