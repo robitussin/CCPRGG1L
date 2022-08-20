e6e18fefc0f2b19e6ac547e6412d84ff94a9a996
robitussin <elizerponiojr@gmail.com>
2022-08-20 19:17:21 +0800

    Added week2
    Added App.java and Character.java

---
+++ week_2/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        mage.name = "Gondar";
+        thief.sayMyName();
+    }
+}
