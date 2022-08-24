c44937c1337e934ae38ac950ecef36223f7fbdcb
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-08-24 14:46:48 +0800

    Create App.java

---
+++ week_2/multiple_class/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+
+        Enemy boss = new Enemy();
+        boss.damage = 50;
+        boss.health = 500;
+        boss.armor = 100;
+        boss.hasMagicImmunnity = true;
+        boss.name = "Slave Knight Gael";
+        boss.enemyDialogue();
+
+        Enemy skeleton = new Enemy();
+        skeleton.damage = 5;
+        skeleton.health = 10;
+        skeleton.armor = 5;
+        skeleton.name = "Common Skeleton Warrior";
+        skeleton.hasMagicImmunnity = false;
+        skeleton.enemyDialogue();
+
+        Enemy dragon = new Enemy();
+        dragon.damage = 40;
+        dragon.health = 400;
+        dragon.armor = 75;
+        dragon.hasMagicImmunnity = false;
+        dragon.name = "Ancient Wyvern";
+        dragon.enemyDialogue();
+    }
+}
