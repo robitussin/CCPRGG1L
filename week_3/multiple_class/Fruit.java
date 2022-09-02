88c7790a8cd3765cbdccb6548a44242fa64bb184
robitussin <elizerponiojr@gmail.com>
2022-09-02 09:44:16 +0800

    Create fruit class

---
+++ week_3/multiple_class/Fruit.java
@@ -0,0 +1,13 @@
+public class Fruit {
+    // properties
+    // color - Yellow
+    String color;
+    // taste - supersweet
+    String taste;
+    // weight - 0.25
+    float weight;
+    // isMyfavorite - true
+    boolean isMyfavorite;
+    // firstLetterofFruitObject - M
+    char firstLetterofFruitObject;
+}
