9e4de961eb47547ad87d52945c3c991352ec7edd
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-09-05 14:53:36 +0800

    Update App.java

--- week_4/operators/logical_operators/App.java
+++ week_4/operators/logical_operators/App.java
@@ -4,17 +4,19 @@ public class App {
         // Logical operators are used to determine the logic between variables or
         // values:
 
-        // Logical and operator (&&)
-        int x = 3;
+        // Logical AND operator (&&)
+        int x = 5;
         System.out.println(x > 1 && x < 5); // returns true because x is greater than 1 and x is less than 5
 
-        // Logical or operator (||)
+        // Logical OR operator (||)
         int y = 5;
         System.out.println(y > 3 || y < 4); // returns true because one of the conditions are true (y is greater than 3
                                             // but y is not less than 4)
 
-        // Logical not operator (!)
+        // Logical NOT operator (!)
         int z = 2;
+        System.out.println(!(z > 5));
+
         System.out.println(!(z > 1 && z < 3)); // returns false because ! (not) is used to reverse the result
     }
 }
