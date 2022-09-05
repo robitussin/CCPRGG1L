53fd02abf46cd7ce0d22e6d26b026fe3dd1600a5
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-09-05 15:21:40 +0800

    Update App.java

--- week_4/string_class/App.java
+++ week_4/string_class/App.java
@@ -1,9 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String myName = new String("Elizer");
+        // Basic way of declaring String variables
         String mySurName = "Ponio";
-
+        
+        // Declaring String variables as Objects
+        String myName = new String("Elizer");
+        
+        // Because String is a class. It has methods that can be accessed.
         System.out.println(myName); // prints Elizer
         System.out.println(myName.length()); // prints 6
         System.out.println(myName.toLowerCase()); // prints elizer
