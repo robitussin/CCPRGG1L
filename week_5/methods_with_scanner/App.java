a0b26d4c0fd085939fcbaf30f7e0d7cd15a744b8
robitussin <elizerponiojr@gmail.com>
2022-09-14 22:11:43 +0800

    Added methods with scanner folder

---
+++ week_5/methods_with_scanner/App.java
@@ -0,0 +1,45 @@
+
+// import the Scanner class
+import java.util.Scanner;
+
+public class App {
+
+    // Scanner Object
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int sum = add();
+        System.out.println("The sum is: " + sum);
+
+        int difference = subtract();
+        System.out.println("The difference is: " + difference);
+
+        // close scanner
+        scan.close();
+    }
+
+    // This method will request for two numbers and will add them.
+    static int add() {
+        System.out.println("This method will add two numbers from user input");
+        System.out.print("Enter the first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int y = scan.nextInt();
+
+        return x + y;
+    }
+
+    // This method will request for two numbers and will subtract them.
+    static int subtract() {
+        System.out.println("This method will subtract two numbers from user input");
+        System.out.print("Enter the first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int y = scan.nextInt();
+
+        return x - y;
+    }
+}
