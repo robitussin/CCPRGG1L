6d481726b0b75760d656352ac09b8edd2985b4c2
robitussin <elizerponiojr@gmail.com>
2022-09-15 10:09:12 +0800

    Updated method with scanner

--- week_5/methods_with_scanner/App.java
+++ week_5/methods_with_scanner/App.java
@@ -17,31 +17,73 @@ public class App {
         int difference = subtract();
         System.out.println("The difference is: " + difference);
 
-        // close scanner
-        scan.close();
+        // Method call
+        int age = computeAge();
+        System.out.println("Your age is: " + age);
+
+        // Method call
+        String crushName = revealMyCrush();
+        System.out.println(crushName);
+
+        // Method call
+        double myGrade = getAverage();
+        System.out.println("My final grade is: " + myGrade + "%");
     }
 
-    // This method will request for two numbers and will add them.
     static int add() {
-        System.out.println("This method will add two numbers from user input");
-        System.out.print("Enter the first number: ");
+        System.out.println("Please enter two numbers to add");
+        System.out.print("Enter first number: ");
         int x = scan.nextInt();
 
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter second number: ");
         int y = scan.nextInt();
-
         return x + y;
     }
 
-    // This method will request for two numbers and will subtract them.
     static int subtract() {
-        System.out.println("This method will subtract two numbers from user input");
-        System.out.print("Enter the first number: ");
+        System.out.println("Please enter two numbers to subtract");
+        System.out.print("Enter first number: ");
         int x = scan.nextInt();
 
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter second number: ");
         int y = scan.nextInt();
-
         return x - y;
+    }
+
+    static int computeAge() {
+        System.out.println("Please enter current year and year of birth");
+        System.out.print("Enter current year: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter the year of your birthday: ");
+        int y = scan.nextInt();
+        return x - y;
+    }
+
+    static String revealMyCrush() {
+        System.out.println("This will reveal the name of your crush");
+        System.out.print("Enter the your name: ");
+
+        String myName = scan.nextLine();
+
+        System.out.print("Enter the name of your crush: ");
+
+        String nameOfMyCrush = scan.nextLine();
+
+        return myName + " loves " + nameOfMyCrush;
+    }
+
+    static double getAverage() {
+        System.out.println("This will compute your final grade");
+        System.out.print("Enter score in exam 1: ");
+        double x = scan.nextDouble();
+
+        System.out.print("Enter score in exam 2: ");
+        double y = scan.nextDouble();
+
+        System.out.print("Enter score in exam 3: ");
+        double z = scan.nextDouble();
+
+        return (x + y + z) / 3;
     }
 }
