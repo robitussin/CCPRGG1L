0723ec1e47ecedf94797acd25b52e813221766a1
robitussin <elizerponiojr@gmail.com>
2022-09-17 10:02:39 +0800

    added for statement

---
+++ week_6/iteration_structures/for_statement/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 10; i != 0; i--) {
+            System.out.println("Woooooo!.. I can do this all day!");
+        }
+
+    }
+}
