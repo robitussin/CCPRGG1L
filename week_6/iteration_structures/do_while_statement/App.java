c43085084c95ca9a217077f71005559b76888a87
robitussin <elizerponiojr@gmail.com>
2022-09-17 10:05:26 +0800

    added do_while

---
+++ week_6/iteration_structures/do_while_statement/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rideCount = 10;
+        do {
+            System.out.println("Woooooo!.. I can do this all day!");
+            rideCount--;
+        } while (rideCount != 0);
+    }
+}
