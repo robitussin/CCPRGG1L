85090cbe63c40fbab26f562503c6b614dd398f9c
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-09-20 13:21:23 +0800

    Update App.java

--- week_6/dating_app/App.java
+++ week_6/dating_app/App.java
@@ -14,103 +14,149 @@ public class App {
         System.out.println("Swipe Left to ignore or Swipe Right to match!");
         System.out.println(" ");
 
-        System.out.println("1. Cristian Ogena");
+        System.out.println("1. Abigail");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + "Cristian Ogena";
+        if (swipe == 'R') {
+            match = match + "Abigail";
         }
 
-        System.out.println("2. Brix Manzanero");
+        System.out.println("2. Dianne");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Brix Manzanero";
+        if (swipe == 'R') {
+            match = match + " Dianne";
         }
    
-        System.out.println("3. John Divina");
+        System.out.println("3. Fiona");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " John Divina";
+        if (swipe == 'R') {
+            match = match + " Fiona";
         }
  
-        System.out.println("4. John Basilio");
+        System.out.println("4. Zophia");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " John Basilio";
+        if (swipe == 'R') {
+            match = match + " Zophia";
         }
 
-        System.out.println("5. Dhan Tamparong");
+        System.out.println("5. Kyla");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Dhan Tamparong";
+        if (swipe == 'R') {
+            match = match + " Kyla";
         }
 
-        System.out.println("6. Mico Salandanan");
+        System.out.println("6. Marga");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Mico Salandanan";
+        if (swipe == 'R') {
+            match = match + " Marga";
         }
 
-        System.out.println("7. Juan Andaya");
+        System.out.println("7. Cherish");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Juan Andaya";
+        if (swipe == 'R') {
+            match = match + " Cherish";
         }
 
-        System.out.println("8. Gian Colinares");
+        System.out.println("8. Elane");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Gian Colinares";
+        if (swipe == 'R') {
+            match = match + " Elane";
         }
 
-        System.out.println("9. Miko Cruz");
+        System.out.println("9. Kimberly");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Miko Cruz";
+        if (swipe == 'R') {
+            match = match + " Kimberly";
         }
 
-        System.out.println("10. Joseph Bonilla");
+        System.out.println("10. Ashley");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Joseph Bonilla";
+        if (swipe == 'R') {
+            match = match + " Ashley";
         }
        
-        System.out.println("11. Jerico Lim");
+        System.out.println("11. Luisa");
         System.out.println("Swipe left or Swipe Right? (press L or R)");
 
         swipe = scan.next().charAt(0);
         
-        if (swipe == 'L') {
-            match = match + " Jerico Lim";
+        if (swipe == 'R') {
+            match = match + " Luisa";
+        }
+
+        System.out.println("12. Thea");
+        System.out.println("Swipe left or Swipe Right? (press L or R)");
+
+        swipe = scan.next().charAt(0);
+        
+        if (swipe == 'R') {
+            match = match + " Thea";
+        }
+
+        System.out.println("13. Fitz");
+        System.out.println("Swipe left or Swipe Right? (press L or R)");
+
+        swipe = scan.next().charAt(0);
+        
+        if (swipe == 'R') {
+            match = match + " Fitz";
+        }
+        
+
+        System.out.println("14. Peri");
+        System.out.println("Swipe left or Swipe Right? (press L or R)");
+
+        swipe = scan.next().charAt(0);
+        
+        if (swipe == 'R') {
+            match = match + " Peri";
+        }
+
+        System.out.println("14. Richard");
+        System.out.println("Swipe left or Swipe Right? (press L or R)");
+
+        swipe = scan.next().charAt(0);
+        
+        if (swipe == 'R') {
+            match = match + " Richard";
+        }
+
+        System.out.println("15. Marc");
+        System.out.println("Swipe left or Swipe Right? (press L or R)");
+
+        swipe = scan.next().charAt(0);
+        
+        if (swipe == 'R') {
+            match = match + " Marc";
         }
 
         System.out.println("You will be matched with: " + match + " if they also swipe you right :)");
