be93442da3311223427cff567f1e8001ef0ce762
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-08 15:26:36 +0800

    Update Answer.java

--- exam_practice_problems/practice_set_2/Answer.java
+++ exam_practice_problems/practice_set_2/Answer.java
@@ -113,9 +113,9 @@ public class Answer {
         // 10. Create a condition to detect if a word is present in the input.
         scan.nextLine();
         System.out.print("Use the word \"love\" in any sentence: ");
-        String love = scan.nextLine();
+        String sentence = scan.nextLine();
 
-        if (love.contains("love")) {
+        if (sentence.contains("love")) {
             System.out.println("valid");
         } else {
             System.out.println("invalid");
