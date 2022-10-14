17a38de9b1376dca397e3508133bf812a129b1aa
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-14 08:19:19 +0800

    Update App.java

--- week_8/for/App.java
+++ week_8/for/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // Prints name 5 times
         for (int i = 5; i != 0; i--) {
-            System.out.println("Woooooo!.. I can do this all day!");
+            System.out.println("Elizer");
         }
 
     }
