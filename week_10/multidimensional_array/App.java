f27ceb5f649b7441b69dbc648f6213f4088d6be5
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-20 10:19:46 +0800

    Update App.java

--- week_10/multidimensional_array/App.java
+++ week_10/multidimensional_array/App.java
@@ -16,5 +16,17 @@ public class App {
 
         // Access element that is selected
         System.out.println(movieReservation[6][6]);
+        
+         // 2x1
+        String[][] Array_2x1 = { { "X", }, { "O" } };
+
+        // 2x2
+        String[][] Array_2x2 = { { "X", "O" }, { "O", "X" } };
+
+        // 2x3
+        String[][] Array_2x3 = { { "X", "O", "X" }, { "O", "X", "O" } };
+
+        // 3x3
+        String[][] Array_3x3 = { { "X", "O", "X" }, { "O", "X", "O" }, { "X", "O","X" } };
     }
 }
