a804d460451c1306612d4b0eaf32950d00b8b3d8
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-25 07:49:22 +0800

    Create exercise.java

---
+++ exercise/word_square/exercise.java
@@ -0,0 +1,40 @@
+public class Exercise {
+    public static void main(String[] args) throws Exception {
+
+        char [][] mdArray = {{'P','R','E','Y'}, {'L','A','V', 'A'}, {'O','V','E', 'R'}, {'T', 'E', 'N', 'D'}};
+    
+
+        // WORD 1
+        System.out.println("1. WORD 1");
+        
+        // WORD 2
+        System.out.println("");
+        System.out.println("2. WORD 2");
+        
+        // WORD 3
+        System.out.println("");
+        System.out.println("3. WORD 3");
+        
+        // WORD 4
+        System.out.println("");
+        System.out.println("4. WORD 4");
+        
+        // WORD 5
+        System.out.println("");
+        System.out.println("5. WORD 5");
+      
+        // WORD 6
+        System.out.println("");
+        System.out.println("3. WORD 6");
+        
+        // WORD 7
+        System.out.println("");
+        System.out.println("4. WORD 7");
+        
+        // WORD 8
+        System.out.println("");
+        System.out.println("5. WORD 8");
+    
+        }
+    }
+}
