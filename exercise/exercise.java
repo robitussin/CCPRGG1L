5ec9db154c93723379dabda080def89dfa8c41ad
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-25 07:06:41 +0800

    Update exercise.java

--- exercise/exercise.java
+++ exercise/exercise.java
@@ -6,16 +6,22 @@ public class App {
 
         // PATTERN 1
         System.out.println("1. PATTERN 1");
+        
         // PATTERN 2
+        System.out.println("");
         System.out.println("2. PATTERN 2");
+        
         // PATTERN 3
+        System.out.println("");
         System.out.println("3. PATTERN 3");
+        
         // PATTERN 4
+        System.out.println("");
         System.out.println("4. PATTERN 4");
-        // PATTERN 5
-        System.out.println("5. PATTERN 5");
+        
         // Get the SUM
-        System.out.println("6. SUM");
+        System.out.println("");
+        System.out.println("5. SUM");
     
         }
     }
