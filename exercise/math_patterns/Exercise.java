fd63a9de0952105e2e0a64c0eb81b8bbb451f7d4
Elizer Ponio Jr <elizerponiojr@gmail.com>
2022-10-27 13:39:36 +0800

    Create Exercise.java

---
+++ exercise/math_patterns/Exercise.java
@@ -0,0 +1,26 @@
+public class Exercise {
+  public static void main(String[] args) throws Exception {
+
+      int[][] mdArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15 ,16}};
+  
+
+      // PATTERN 1
+      System.out.println("1. SUM OF PATTERN 1");
+      
+      // PATTERN 2
+      System.out.println("");
+      System.out.println("2. SUM OF PATTERN 2");
+      
+      // PATTERN 3
+      System.out.println("");
+      System.out.println("3. SUM OF PATTERN 3");
+      
+      // PATTERN 4
+      System.out.println("");
+      System.out.println("4. SUM OF PATTERN 4");
+      
+      // PATTERN 5
+      System.out.println("");
+      System.out.println("5. SUM OF PATTERN 5");
+  }
+}
